d3e24205e1016fddb3f053f6a91411d35417af7c
adranwit <adrianwit@gmail.com>
2024-07-19 09:29:54 -0700

    updated metadata

--- meta.go
+++ meta.go
@@ -5,6 +5,7 @@ import (
 	"database/sql/driver"
 	"fmt"
 	as "github.com/aerospike/aerospike-client-go/v6"
+	"reflect"
 	"strings"
 )
 
@@ -17,12 +18,31 @@ type table struct {
 	TableName   string `sqlx:"table_name" aerospike:"table_name"`
 }
 
+type tableColumn struct {
+	TableSchema            string `sqlx:"table_schema" aerospike:"table_schema"`
+	TableName              string `sqlx:"table_name" aerospike:"table_name,pk=true"`
+	ColumnName             string `sqlx:"column_name" aerospike:"column_name"`
+	OrdinalPosition        int    `sqlx:"ordinal_position" aerospike:"ordinal_position"`
+	ColumnComment          string `sqlx:"column_comment" aerospike:"column_comment"`
+	DataType               string `sqlx:"data_type" aerospike:"data_type"`
+	CharacterMaximumLength int    `sqlx:"character_maximum_length" aerospike:"character_maximum_length"`
+	NumericPrecision       int    `sqlx:"numeric_precision" aerospike:"numeric_precision"`
+	NumericScale           int    `sqlx:"numeric_scale" aerospike:"numeric_scale"`
+	IsNullable             string `sqlx:"is_nullable" aerospike:"is_nullable"`
+	ColumnDefault          string `sqlx:"column_default" aerospike:"column_default"`
+	ColumnKey              string `sqlx:"column_key" aerospike:"column_key"`
+	IsAutoIncrement        int    `sqlx:"is_autoincrement" aerospike:"is_autoincrement"`
+}
+
 func (s *Statement) handleInformationSchema(ctx context.Context, keys []*as.Key, rows *Rows) (driver.Rows, error) {
 	switch strings.ToLower(s.set) {
 	case "schemata":
 		return s.handleSchemaSet(ctx, keys, rows)
 	case "tables":
 		return s.handleTableSet(ctx, keys, rows)
+	case "columns":
+		return s.handleTableInfo(ctx, keys, rows)
+
 	}
 
 	return nil, fmt.Errorf("unsupported InformationSchema: %v", s.set)
@@ -68,60 +88,67 @@ func (s *Statement) handleSchemaSet(ctx context.Context, keys []*as.Key, rows *R
 	return rows, nil
 }
 
+func (s *Statement) handleTableInfo(ctx context.Context, keys []*as.Key, rows *Rows) (driver.Rows, error) {
+	indexedKeys := map[string]bool{}
+	for _, key := range keys {
+		indexedKeys[key.Value().GetObject().(string)] = true
+	}
+
+	var qualified []*set
+	recs := make([]*as.Record, 0)
+	for _, item := range s.sets.sets() {
+		if len(indexedKeys) > 0 && !indexedKeys[item] {
+			continue
+		}
+		aSet := s.sets.Lookup(item)
+		qualified = append(qualified, aSet)
+	}
+	for _, aSet := range qualified {
+		xType := aSet.xType.Type
+		for i := 0; i < xType.NumField(); i++ {
+			aField := xType.Field(i)
+			aTag, err := ParseTag(aField.Tag.Get("aerospike"))
+			if err != nil {
+				return nil, err
+			}
+			if aTag.Name == "" {
+				aTag.Name = aField.Name
+			}
+			rec := &as.Record{
+				Bins: as.BinMap{
+					"table_schema":             s.cfg.namespace,
+					"table_name":               aSet.xType.Name,
+					"column_name":              aTag.Name,
+					"ordinal_position":         i,
+					"column_comment":           "",
+					"data_type":                aField.Type.String(),
+					"character_maximum_length": 0,
+					"numeric_precision":        0,
+					"numeric_scale":            0,
+					"is_nullable":              aField.Type.Kind() == reflect.Ptr,
+					"column_default":           "",
+					"column_key":               "",
+					"is_autoincrement":         "",
+				},
+			}
+			recs = append(recs, rec)
+		}
+	}
+	rows.rowsReader = newRowsReader(recs)
+	return rows, nil
+}
+
 func (s *Statement) handleTableSet(ctx context.Context, keys []*as.Key, rows *Rows) (driver.Rows, error) {
 	indexedKeys := map[string]bool{}
 	for _, key := range keys {
 		indexedKeys[key.Value().GetObject().(string)] = true
 	}
 
-	aNodes := s.client.Cluster().GetNodes()
-	if len(aNodes) == 0 {
-		return nil, fmt.Errorf("no nodes available")
-	}
-	aNode := aNodes[0]
-	policy := as.NewInfoPolicy()
-	namespaceMap, err := aNode.RequestInfo(policy, "sets")
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve namespaces: %v", err)
-	}
-	values, _ := namespaceMap["sets"]
-
-	/* example value with \n added
-	ns=test:set=AAA01:objects=2:tombstones=0:memory_data_bytes=0:device_data_bytes=544:truncate_lut=0:sindexes=1:index_populating=false:disable-eviction=false:enable-index=false:stop-writes-count=0;
-	ns=test:set=AAA02:objects=10:tombstones=0:memory_data_bytes=0:device_data_bytes=1120:truncate_lut=0:sindexes=1:index_populating=false:disable-eviction=false:enable-index=false:stop-writes-count=0;
-	*/
-
-	// TODO add sets from registries
-	// TODO what to do if registry vs db set collision?
 	recs := make([]*as.Record, 0)
-	//for _, line := range strings.Split(values, ";") {
-	//	parts := strings.Split(line, ":")
-	//	if len(parts) < 2 {
-	//		continue
-	//	}
-	//
-	//	namespace := ""
-	//	idx := strings.Index(parts[0], "=")
-	//	if idx+1 <= len(parts[0])-1 {
-	//		namespace = parts[0][idx+1:]
-	//	}
-	//
-	//	set := ""
-	//	idx = strings.Index(parts[1], "=")
-	//	if idx+1 <= len(parts[1])-1 {
-	//		set = parts[1][idx+1:]
-	//	}
-	//
-	//	rec := &as.Record{
-	//		Bins: as.BinMap{
-	//			"table_schema": namespace,
-	//			"table_name":   set,
-	//		},
-	//	}
-	//	recs = append(recs, rec)
-	//}
-
 	for _, item := range s.sets.sets() {
+		if len(indexedKeys) > 0 && !indexedKeys[item] {
+			continue
+		}
 		rec := &as.Record{
 			Bins: as.BinMap{
 				"table_schema": s.namespace,
