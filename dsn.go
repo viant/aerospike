7b13b008c51c47a1042dfb4cf38f6495b55592e7
pol-mfilipowicz <pol-mfilipowicz@viant-contractors.com>
2024-11-07 19:38:43 +0100

    added more clientPolicy params

--- dsn.go
+++ dsn.go
@@ -6,19 +6,28 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // Config represent Connection config
 type Config struct {
-	host                string
-	port                int
-	namespace           string
-	ClientPolicy        *as.ClientPolicy
-	batchSize           int
-	concurrency         int
-	maxConcurrentWrite  int
-	connectionQueueSize int
-	Values              url.Values
+	host                       string
+	port                       int
+	namespace                  string
+	ClientPolicy               *as.ClientPolicy
+	batchSize                  int
+	concurrency                int
+	maxConcurrentWrite         int
+	connectionQueueSize        int
+	Values                     url.Values
+	timeout                    time.Duration
+	idleTimeout                time.Duration
+	loginTimeout               time.Duration
+	openingConnectionThreshold int
+	tendInterval               time.Duration
+	maxErrorRate               int
+	errorRateWindow            int
+
 	// expiry options
 	/*
 		Key     string
@@ -92,10 +101,104 @@ func ParseDSN(dsn string) (*Config, error) {
 				return nil, fmt.Errorf("invalid dsn batchSize: %v", err)
 			}
 		}
+
+		if v, ok := cfg.Values["timeout"]; ok {
+			if cfg.timeout, err = time.ParseDuration(v[0] + "s"); err != nil {
+				return nil, fmt.Errorf("invalid dsn timeout: %v", err)
+			}
+		}
+
+		if v, ok := cfg.Values["idleTimeout"]; ok {
+			if cfg.idleTimeout, err = time.ParseDuration(v[0] + "s"); err != nil {
+				return nil, fmt.Errorf("invalid dsn idleTimeout: %v", err)
+			}
+		}
+
+		if v, ok := cfg.Values["loginTimeout"]; ok {
+			if cfg.loginTimeout, err = time.ParseDuration(v[0] + "s"); err != nil {
+				return nil, fmt.Errorf("invalid dsn loginTimeout: %v", err)
+			}
+		}
+
+		if v, ok := cfg.Values["openingConnectionThreshold"]; ok {
+			if cfg.openingConnectionThreshold, err = strconv.Atoi(v[0]); err != nil {
+				return nil, fmt.Errorf("invalid dsn openingConnectionThreshold: %v", err)
+			}
+		}
+
+		//if v, ok := cfg.Values["failIfNotConnected"]; ok {
+		//	if cfg.failIfNotConnected, err = strconv.ParseBool(v[0]); err != nil {
+		//		return nil, fmt.Errorf("invalid dsn failIfNotConnected: %v", err)
+		//	}
+		//}
+
+		if v, ok := cfg.Values["tendInterval"]; ok {
+			if cfg.tendInterval, err = time.ParseDuration(v[0] + "s"); err != nil {
+				return nil, fmt.Errorf("invalid dsn tendInterval: %v", err)
+			}
+		}
+
+		//if v, ok := cfg.Values["limitConnectionsToQueueSize"]; ok {
+		//	if cfg.limitConnectionsToQueueSize, err = strconv.ParseBool(v[0]); err != nil {
+		//		return nil, fmt.Errorf("invalid dsn limitConnectionsToQueueSize: %v", err)
+		//	}
+		//}
+		//
+		//if v, ok := cfg.Values["ignoreOtherSubnetAliases"]; ok {
+		//	if cfg.ignoreOtherSubnetAliases, err = strconv.ParseBool(v[0]); err != nil {
+		//		return nil, fmt.Errorf("invalid dsn ignoreOtherSubnetAliases: %v", err)
+		//	}
+		//}
+
+		if v, ok := cfg.Values["maxErrorRate"]; ok {
+			if cfg.maxErrorRate, err = strconv.Atoi(v[0]); err != nil {
+				return nil, fmt.Errorf("invalid dsn maxErrorRate: %v", err)
+			}
+		}
+
+		if v, ok := cfg.Values["errorRateWindow"]; ok {
+			if cfg.errorRateWindow, err = strconv.Atoi(v[0]); err != nil {
+				return nil, fmt.Errorf("invalid dsn errorRateWindow: %v", err)
+			}
+		}
+
+		//if v, ok := cfg.Values["seedOnlyCluster"]; ok {
+		//	if cfg.seedOnlyCluster, err = strconv.ParseBool(v[0]); err != nil {
+		//		return nil, fmt.Errorf("invalid dsn seedOnlyCluster: %v", err)
+		//	}
+		//}
 	}
 
 	if cfg.connectionQueueSize != 0 {
 		cfg.ClientPolicy.ConnectionQueueSize = cfg.connectionQueueSize
+	}
+
+	if cfg.timeout != 0 {
+		cfg.ClientPolicy.Timeout = cfg.timeout
+	}
+
+	if cfg.idleTimeout != 0 {
+		cfg.ClientPolicy.IdleTimeout = cfg.idleTimeout
+	}
+
+	if cfg.loginTimeout != 0 {
+		cfg.ClientPolicy.LoginTimeout = cfg.loginTimeout
+	}
+
+	if cfg.openingConnectionThreshold != 0 {
+		cfg.ClientPolicy.OpeningConnectionThreshold = cfg.openingConnectionThreshold
+	}
+
+	if cfg.tendInterval != 0 {
+		cfg.ClientPolicy.TendInterval = cfg.tendInterval
+	}
+
+	if cfg.maxErrorRate != 0 {
+		cfg.ClientPolicy.MaxErrorRate = cfg.maxErrorRate
+	}
+
+	if cfg.errorRateWindow != 0 {
+		cfg.ClientPolicy.ErrorRateWindow = cfg.errorRateWindow
 	}
 
 	if isDebugOn() {
--- dsn_test.go
+++ dsn_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/viant/toolbox"
 	"net/url"
 	"testing"
+	"time"
 )
 
 func Test_ParseDSN(t *testing.T) {
@@ -33,20 +34,48 @@ func Test_ParseDSN(t *testing.T) {
 		},
 		{
 			description: "dsn just with all params",
-			dsn:         "aerospike://127.0.0.1:3000/namespace_abc?concurrency=10&maxConcurrentWrite=5&batchSize=100&connectionQueueSize=200",
+			dsn:         "aerospike://127.0.0.1:3000/namespace_abcd?concurrency=10&maxConcurrentWrite=5&batchSize=100&connectionQueueSize=200&timeout=66&idleTimeout=11&loginTimeout=33&openingConnectionThreshold=550&tendInterval=15&maxErrorRate=56&errorRateWindow=7",
 			expect: &Config{
-				host:               "127.0.0.1",
-				port:               3000,
-				namespace:          "namespace_abc",
-				ClientPolicy:       as.NewClientPolicy(),
-				batchSize:          100,
-				concurrency:        10,
-				maxConcurrentWrite: 5,
+				host:      "127.0.0.1",
+				port:      3000,
+				namespace: "namespace_abcd",
+				ClientPolicy: &as.ClientPolicy{
+					AuthMode:                    as.AuthModeInternal,
+					Timeout:                     66 * time.Second,
+					IdleTimeout:                 11 * time.Second,
+					LoginTimeout:                33 * time.Second,
+					ConnectionQueueSize:         200,
+					OpeningConnectionThreshold:  550,
+					FailIfNotConnected:          true,
+					TendInterval:                15 * time.Second,
+					LimitConnectionsToQueueSize: true,
+					IgnoreOtherSubnetAliases:    false,
+					MaxErrorRate:                56,
+					ErrorRateWindow:             7,
+					SeedOnlyCluster:             false,
+				},
+				batchSize:                  100,
+				concurrency:                10,
+				maxConcurrentWrite:         5,
+				timeout:                    66 * time.Second,
+				idleTimeout:                11 * time.Second,
+				loginTimeout:               33 * time.Second,
+				openingConnectionThreshold: 550,
+				tendInterval:               15 * time.Second,
+				maxErrorRate:               56,
+				errorRateWindow:            7,
 				Values: url.Values{
-					"batchSize":           []string{"100"},
-					"concurrency":         []string{"10"},
-					"connectionQueueSize": []string{"200"},
-					"maxConcurrentWrite":  []string{"5"},
+					"batchSize":                  []string{"100"},
+					"concurrency":                []string{"10"},
+					"connectionQueueSize":        []string{"200"},
+					"maxConcurrentWrite":         []string{"5"},
+					"timeout":                    []string{"66"},
+					"idleTimeout":                []string{"11"},
+					"loginTimeout":               []string{"33"},
+					"openingConnectionThreshold": []string{"550"},
+					"tendInterval":               []string{"15"},
+					"maxErrorRate":               []string{"56"},
+					"errorRateWindow":            []string{"7"},
 				},
 				connectionQueueSize: 200,
 			},
