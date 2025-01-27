6df2e410419cc9c81deed8cb71ad971467d06e30
pol-mfilipowicz <pol-mfilipowicz@viant-contractors.com>
2025-01-27 13:06:48 +0100

    fixed concurrent map writes

--- set.go
+++ set.go
@@ -1,22 +1,30 @@
 package aerospike
 
-import "github.com/viant/x"
+import (
+	"github.com/viant/x"
+	"sync"
+)
 
 type set struct {
 	xType           *x.Type
 	typeBasedMapper *mapper
 	queryMapper     map[string]*mapper
 	ttlSec          uint32
+	mux             sync.RWMutex
 }
 
 func (s *set) lookupQueryMapper(query string) *mapper {
 	if len(s.queryMapper) == 0 {
 		return nil
 	}
+	s.mux.RLock()
+	defer s.mux.RUnlock()
 	return s.queryMapper[query]
 }
 
 func (s *set) registerQueryMapper(query string, aMapper *mapper) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	if len(s.queryMapper) == 0 {
 		s.queryMapper = make(map[string]*mapper)
 	}
